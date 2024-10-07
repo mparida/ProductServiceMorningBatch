456c1e128060dad26144e43c14fa5d12f2af59f3
mp1863 <mp1863@att.com>
2024-10-07 18:03:08 +0530

    service32

--- src/main/java/com/scaler/productservicemorningbatch/services/FakeStoreProductService.java
+++ src/main/java/com/scaler/productservicemorningbatch/services/FakeStoreProductService.java
@@ -1,5 +1,6 @@
 package com.scaler.productservicemorningbatch.services;
 
+import com.scaler.productservicemorningbatch.dtos.ProductDto;
 import com.scaler.productservicemorningbatch.models.Product;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -7,10 +8,14 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 @Service
 public class FakeStoreProductService implements ProductService {
+    private  RestTemplate restTemplate;
+    FakeStoreProductService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
     @Override
     public Product getProductById(Long id) {
         //Call Fake store api to get the product with given Id
-        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getForObject("https://fakestoreapi.com/products" + id, ProductDto.class);
         return null;
     }
 
